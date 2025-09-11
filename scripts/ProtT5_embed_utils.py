1f7b31b49f1a216b968e81adb0f5602a97bc4cc6
Charlotte Li <75584616+pikanaeri@users.noreply.github.com>
2025-09-11 14:06:41 -0400

    Update ProtT5_embed_utils.py

--- scripts/ProtT5_embed_utils.py
+++ scripts/ProtT5_embed_utils.py
@@ -45,16 +45,9 @@ def prott5_xl_uniref50_embed(faa_path: str, max_length: int, num_gpus: int, batc
 
     identifiers, sequences = _get_faa(faa_path, max_length=max_length)
 
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') ## ProtT5 only appears to support 1 GPU, in half precision
-    print("Using device: {}".format(device))
-    transformer_link = "Rostlab/prot_t5_xl_half_uniref50-enc"
-    print("Loading: {}".format(transformer_link))
-    model = T5EncoderModel.from_pretrained(transformer_link)
-    model.full() if device=='cpu' else model.half()
-    model = model.to(device)
-    model = model.eval()
-    
-    tokenizer = T5Tokenizer.from_pretrained(transformer_link, do_lower_case=False )
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_half_uniref50-enc', do_lower_case=False)
+    model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc").to(device)
 
     ## batch sequence embedding to reduce memory
     d = {}
